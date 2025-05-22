236d2d7bd407195e0e94435d4f1be1fea93e6150
sportshead <me@sportshead.dev>
2025-05-22 20:24:44 +0100

    feat: add snacks plugin

--- config/plugins/ui.nix
+++ config/plugins/ui.nix
@@ -16,4 +16,27 @@
       };
     };
   };
+
+  plugins.snacks = {
+    enable = true;
+    settings = {
+      input.enabled = true;
+      picker.enabled = true;
+      rename.enabled = true;
+    };
+  };
+
+  autoCmd = [
+    {
+      event = "User";
+      pattern = "OilActionsPost";
+      callback.__raw = ''
+        function(event)
+          if event.data.actions.type == "move" then
+            Snacks.rename.on_rename_file(event.data.actions.src_url, event.data.actions.dest_url)
+          end
+        end
+      '';
+    }
+  ];
 }
