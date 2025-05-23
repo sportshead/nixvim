bdefc0d41db7995d9783cebaf954cda233f28cd2
sportshead <me@sportshead.dev>
2025-05-23 17:26:22 +0100

    fix: clipboard-image

--- config/plugins/clipboard-image.nix
+++ config/plugins/clipboard-image.nix
@@ -1,14 +1,22 @@
-{
-  pkgs,
-  lib,
-  ...
-}: let
-  inherit (lib) mkIf stdenv;
+{pkgs, ...}: let
+  inherit (pkgs) lib stdenv;
 in {
   plugins.clipboard-image = {
     enable = true;
-    clipboardPackage = mkIf stdenv.isDarwin pkgs.pngpaste;
+    clipboardPackage = lib.mkIf stdenv.isDarwin pkgs.pngpaste;
 
     settings.img_name.__raw = "function() return os.date('%Y%m%dT%H%M%S') end";
+
+    lazyLoad = {
+      enable = true;
+      settings = {
+        keys = [
+          {
+            __unkeyed-1 = "<leader>v";
+            __unkeyed-2 = "<Cmd>PasteImg<CR>";
+          }
+        ];
+      };
+    };
   };
 }
